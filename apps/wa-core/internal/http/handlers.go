bc7db51aa912017e3045ca482b1b4bbd865878e6
taipgonesistema-cloud <taipgonesistema@gmail.com>
2026-03-28 13:05:05 -0300

    allow put requests for kanban cors

--- apps/wa-core/internal/http/handlers.go
+++ apps/wa-core/internal/http/handlers.go
@@ -91,7 +91,7 @@ func (a *API) cors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS")
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusNoContent)
 			return
